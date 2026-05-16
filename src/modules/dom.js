064f937fd5b2c2e3f107e8e4cf446e2da336ef5d
[200~155949712+Katusyo@users.noreply.github.com~ <155949712+Katusyo@users.noreply.github.com>
2026-05-16 10:09:23 +0900

    Progress of Todo List Project
    + Initial commit with project structure and basic files.
    + Added webpack configuration and basic HTML, CSS, and
    JavaScript files.
    + Implemented basic Todo and Project classes.
    + Set up development environment with npm scripts for
    building and serving the project.
    + Added .gitignore to exclude node_modules and dist
    folders from version control.
    + Created basic DOM manipulation

---
+++ src/modules/dom.js
@@ -0,0 +1,14 @@
+[
+    {
+        name: 'default',
+        todos: [
+            {
+                title: 'Enter project name',
+                description: 'Create a new project by entering a name and clicking the "Add Project" button.',
+                dueDate: 'N/A',
+                priority: 'N/A',
+                completed: false,
+            },
+        ],
+    },
+];
