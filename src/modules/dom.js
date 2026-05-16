677ae8bb00dd40ee02d1cf091667165c20427bdc
[200~155949712+Katusyo@users.noreply.github.com~ <155949712+Katusyo@users.noreply.github.com>
2026-05-16 10:35:53 +0900

    Progress of TOdo List project
    + DOM module rendering functions created

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,14 +1,49 @@
-[
-    {
-        name: 'default',
-        todos: [
-            {
-                title: 'Enter project name',
-                description: 'Create a new project by entering a name and clicking the "Add Project" button.',
-                dueDate: 'N/A',
-                priority: 'N/A',
-                completed: false,
-            },
-        ],
-    },
-];
+const content = document.getElementById('content');
+
+function renderProjects(projects) {
+    content.innerHTML = '';
+
+    projects.forEach((project) => {
+        const projectDiv = document.createElement('div');
+        projectDiv.classList.add('project');
+
+        const projectTitle = document.createElement('h2');
+        projectTitle.textContent = project.name;
+
+        projectDiv.appendChild(projectTitle);
+
+        renderTodos(project, projectDiv);
+
+        content.appendChild(projectDiv);
+    });
+}
+
+function renderTodos(project, container) {
+    project.todos.forEach((todo) => {
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo-card');
+
+        const title = document.createElement('h3');
+        title.textContent = todo.title;
+
+        const description = document.createElement('p');
+        description.textContent = todo.description;
+
+        const dueDate = document.createElement('p');
+        dueDate.textContent = `Due: ${todo.dueDate}`;
+
+        const priority = document.createElement('p');
+        priority.textContent = `Priority: ${todo.priority}`;
+        
+        const status =document.createElement('p');
+        status.textContent = todo.completed
+            ? 'Completed'
+            : 'Not Complete';
+        
+        todoDiv.append(title, description, dueDate, priority, status);
+
+        container.appendChild(todoDiv);
+    });
+}
+
+export default renderProjects;
--- src/modules/project.js
+++ src/modules/project.js
@@ -8,3 +8,18 @@ export default class Project {
         this.todos.push(todo);
     }
 }
+
+[
+    {
+        name: 'default',
+        todos: [
+            {
+                title: 'Enter project name',
+                description: 'Create a new project by entering a name and clicking the "Add Project" button.',
+                dueDate: 'N/A',
+                priority: 'N/A',
+                completed: false,
+            },
+        ],
+    },
+];
