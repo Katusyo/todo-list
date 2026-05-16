514757544afd693132f1d930bf898938037fdbf3
[200~155949712+Katusyo@users.noreply.github.com~ <155949712+Katusyo@users.noreply.github.com>
2026-05-16 10:53:21 +0900

    Progess of Todo List Project
    + Render a list of todo items on the page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
 
-console.log('Todo app running');
+import Project from './modules/project';
+import Todo from './modules/todo';
+import renderProjects from './modules/dom';
+
+const defaultProject = new Project('default');
+
+defaultProject.addTodo(
+    new Todo(
+        'add project name',
+        'Create a new project by entering a name and clicking the "Add Project" button.',
+        'N/A',
+        'N/A'
+    )
+);
+
+const projects = [defaultProject];
+
+renderProjects(projects);
