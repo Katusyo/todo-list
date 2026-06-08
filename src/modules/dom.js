3517bb17bb978e1c94e93572ab51334019dd6f1b
[200~155949712+Katusyo@users.noreply.github.com~ <155949712+Katusyo@users.noreply.github.com>
2026-06-08 09:28:50 +0900

    Progress of todo list project
    + due today view included
    + changed layout to include tasks due today in the today view
    + overdue tasks view included
    + changed layout to include tasks that are overde to show in the overdue
        view
    + cleaned up unnecessary code
    + fixed a problem with the add task and delete project button
        overlapping

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,9 +1,14 @@
 import Project from './project';
 import Todo from './todo';
+import logo from '../assets/vector-square-plus.png';
 import { saveProjects } from './storage';
 import { format } from 'date-fns';
 
 let currentActiveProject = null;
+
+let currentSort = 'default';
+
+let currentView = 'project';
 
 let editingTodo = null;
 
@@ -85,6 +90,7 @@ let currentProjects = [];
 
 function renderProjects(projects, activeProject) {
     currentProjects = projects;
+    currentView = 'project';
 
     content.innerHTML = '';
 
@@ -107,6 +113,8 @@ function renderProjects(projects, activeProject) {
 
     overlay.classList.remove('hidden');
 });
+
+
 
     const projectDiv = document.createElement('div');
     projectDiv.classList.add('project');
@@ -143,7 +151,24 @@ function renderProjects(projects, activeProject) {
     const actionsDiv = document.createElement('div');
     actionsDiv.classList.add('project-actions');
 
-    actionsDiv.append(deleteProjectBtn, addTaskBtn);
+    const sortSelect = document.createElement('select');
+
+    sortSelect.innerHTML = `
+    <option value="default">Default</option>
+    <option value="dueDate">Due Date</option>
+    <option value="priority">Priority</option>
+    <option value="overdue">Overdue First</option>
+    `;
+
+    sortSelect.value = currentSort;
+
+    sortSelect.addEventListener('change', () => {
+        currentSort = sortSelect.value;
+
+        renderProjects(projects, activeProject);
+    });
+
+    actionsDiv.append(sortSelect, addTaskBtn, deleteProjectBtn);
 
     headerDiv.append(title, actionsDiv);
 
@@ -159,118 +184,252 @@ function renderProjects(projects, activeProject) {
     content.appendChild(projectDiv);
 }
 
-function renderTodos(project, container, projects) {
-    if (!project.todos) {
-        console.error('Project has no todos array');
-        return;
+function renderSingleTodo(
+    todo,
+    project,
+    container,
+    projects
+) {
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo-card');
+
+    if (todo.completed) {
+        todoDiv.classList.add('completed');
     }
-    project.todos.forEach((todo, todoIndex) => {
-        const todoDiv = document.createElement('div');
-        todoDiv.classList.add('todo-card');
 
-        if (todo.completed) {
-            todoDiv.classList.add('completed');
-        }
+    const title = document.createElement('h3');
+    title.textContent = todo.title;
 
-        const title = document.createElement('h3');
-        title.textContent = todo.title;
+    const description = document.createElement('p');
+    description.textContent = todo.description;
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete';
-        deleteBtn.addEventListener('click', () => {
-            project.todos.splice(todoIndex, 1);
+    const dueDate = document.createElement('p');
 
-            saveProjects(projects);
+    let formattedDate = 'No Due Date'
 
-            renderProjects(currentProjects, project);
-        });
-
-        const completeBtn = document.createElement('button');
-        completeBtn.textContent = 'Complete';
-        completeBtn.addEventListener('click', () => {
-            todo.completed = !todo.completed;
-
-            saveProjects(projects);
-
-            renderProjects(projects, project);
-        });
-
-        const editBtn = document.createElement('button');
-        editBtn.textContent = 'Edit';
-        editBtn.addEventListener('click', () => {
-            
-            editingTodo = todo;
-
-            document.getElementById('todo-title').value = todo.title;
-            document.getElementById('todo-description').value = todo.description;
-            document.getElementById('todo-date').value = todo.dueDate;
-            document.getElementById('todo-priority').value = todo.priority;
-
-            taskModal.classList.remove('hidden');
-
-            overlay.classList.remove('hidden');
-        });
-
-
-        const description = document.createElement('p');
-        description.textContent = todo.description;
-
-        const dueDate = document.createElement('p');
-
-        let formattedDate = 'No Due Date';
-
-        if (
-            todo.dueDate && !isNaN(new Date(todo.dueDate))
-        ) {
-            formattedDate = format(
-                new Date(todo.dueDate),
-                'MMM-dd-yyyy'
-            );
-        }
-
-        dueDate.textContent = `Due: ${formattedDate}`;
-
-        const priority = document.createElement('p');
-        priority.textContent = `Priority: ${todo.priority}`;
-
-        priority.classList.add(`priority-${todo.priority.toLowerCase()}`);
-
-        todoDiv.classList.add(todo.priority.toLowerCase());
-        
-        const status = document.createElement('p');
-        status.textContent = todo.completed
-            ? 'Completed'
-            : 'Not Complete';
-
-        const cardActions = document.createElement('div');
-        cardActions.classList.add('todo-actions');
-
-        cardActions.append(
-            completeBtn, editBtn, deleteBtn
+    if (
+        todo.dueDate &&
+        !isNaN(new Date(todo.dueDate))
+    ) {
+        formattedDate = format(
+            new Date(todo.dueDate),
+            'MMM-dd-yyyy'
         );
+    }
 
-        todoDiv.append(
-            title, description, dueDate, priority, status, cardActions
-        );            
+    dueDate.textContent = `Due: ${formattedDate}`;
 
-        container.appendChild(todoDiv);
-    });
-}
+    if (
+        todo.dueDate &&
+        new Date(todo.dueDate) < new Date() &&
+        !todo.completed
+    ) {
+        dueDate.classList.add('overdue');
+    }
 
-function createDeleteButton(project, todoIndex, projects) {
-    const btn = document.createElement('button');
+    const priority = document.createElement('p');
+    priority.textContent = `Priorirty: ${todo.priority}`;
 
-    btn.textContent = 'Delete';
+    priority.classList.add(
+        `priority-${todo.priority.toLowerCase()}`
+    );
 
-    btn.addEventListener('click', () => {
-        project.todos.splice(todoIndex, 1);
+    todoDiv.classList.add(
+        todo.priority.toLowerCase()
+    );
+
+    const status = document.createElement('p');
+    status.textContent = todo.completed
+        ? 'Completed'
+        : 'Not Completed';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+
+    deleteBtn.addEventListener('click', () => {
+        const index = project.todos.indexOf(todo);
+
+        project.todos.splice(index, 1);
 
         saveProjects(projects);
 
         renderProjects(projects, project);
     });
 
-    return btn;
+    const completeBtn = document.createElement('button');
+    completeBtn.textContent = 'Complete';
+
+    completeBtn.addEventListener('click', () => {
+        todo.completed = !todo.completed;
+
+        saveProjects(projects);
+
+        if (currentView === 'today') {
+            renderTodayView(projects);
+        } else if (currentView === 'overdue') {
+            renderOverdueView(projects);
+        } else {
+            renderProjects(projects, project);
+        }
+    });
+
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+
+    editBtn.addEventListener('click', () => {
+        editingTodo = todo;
+
+        document.getElementById('todo-title').value = todo.title;
+
+        document.getElementById('todo-description').value = todo.description;
+
+        document.getElementById('todo-date').value = todo.dueDate;
+
+        document.getElementById('todo-priority').value = todo.priority;
+
+        taskModal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+
+    const cardActions = document.createElement('div');
+    cardActions.classList.add('todo-actions');
+
+    cardActions.append(
+        completeBtn,
+        editBtn,
+        deleteBtn
+    );
+
+    todoDiv.append(
+        title,
+        description,
+        dueDate,
+        priority,
+        status,
+        cardActions
+    );
+
+    container.appendChild(todoDiv);
+}
+
+function renderTodayView(projects) {
+    content.innerHTML = '';
+
+    const title = document.createElement('h2');
+    title.textContent = 'Today';
+
+    content.appendChild(title);
+
+    const todoGrid = document.createElement('div');
+    todoGrid.classList.add('todo-grid');
+
+    content.appendChild(todoGrid);
+
+    const today = new Date().toISOString().split('T')[0];
+
+    projects.forEach((project) => {
+        project.todos.forEach((todo) => {
+            if (
+                todo.dueDate === today && !todo.completed
+            ) {
+                renderSingleTodo(
+                    todo,
+                    project,
+                    todoGrid,
+                    projects
+                );
+            }
+        });
+    });
+};
+
+function renderOverdueView(projects) {
+    content.innerHTML = '';
+
+    const title = document.createElement('h2');
+    title.textContent = 'Overdue';
+
+    content.appendChild(title);
+
+    const todoGrid = document.createElement('div');
+    todoGrid.classList.add('todo-grid');
+
+    content.appendChild(todoGrid);
+
+    const todayString = new Date().toISOString().split('T')[0];
+
+    projects.forEach((project) => {
+        project.todos.forEach((todo) => {
+            if (
+                todo.dueDate &&
+                todo.dueDate < todayString &&
+                !todo.completed
+            ) {
+                renderSingleTodo(
+                    todo,
+                    project,
+                    todoGrid,
+                    projects
+                );
+            }
+        });
+    });
+}
+
+function renderTodos(project, container, projects) {
+    if (!project.todos) {
+        console.error('Project has no todos array');
+        return;
+    }
+
+    let todosToRender = [...project.todos];
+
+    if (currentSort === 'dueDate') {
+        todosToRender.sort((a, b) => {
+            const aDate = a.dueDate
+                ? new Date(a.dueDate)
+                : new Date('9999-12-31');
+
+            const bDate = b.dueDate
+                ? new Date(b.dueDate)
+                : new Date('9999-12-31');
+
+            return aDate - bDate;
+        });
+    }
+
+    const priorityOrder = {
+        High: 3,
+        Medium: 2,
+        Low: 1
+    };
+
+    if (currentSort === 'priority') {
+        todosToRender.sort((a, b) => {
+            return priorityOrder[b.priority] - priorityOrder[a.priority]
+        });
+    }
+
+    if (currentSort === 'overdue') {
+        todosToRender.sort((a, b) => {
+            const today = new Date();
+                
+            const aOverdue = a.dueDate && new Date(a.dueDate) < today;
+
+            const bOverdue = b.dueDate && new Date(b.dueDate) < today;
+
+            return bOverdue - aOverdue;
+        });
+    }
+
+    todosToRender.forEach((todo) => {
+        renderSingleTodo(
+            todo,
+            project,
+            container,
+            projects
+        );
+    });
 }
 
 const sidebar = document.getElementById('sidebar');
@@ -278,13 +437,63 @@ const sidebar = document.getElementById('sidebar');
 function renderProjectList(projects, activeProject) {
     sidebar.innerHTML = '';
 
+    const logoContainer = document.createElement('div');
+    logoContainer.classList.add('sidebar-header');
+
+    const logoImg = document.createElement('img');
+    logoImg.src = logo;
+    logoImg.alt = 'VP Todo Logo';
+    logoImg.classList.add('sidebar-logo');
+
+    const appTitle = document.createElement('h2');
+    appTitle.textContent = '+VP Todo+';
+
+    logoContainer.append(logoImg, appTitle);
+
+    sidebar.appendChild(logoContainer);
+
+    const todayBtn = document.createElement('button');
+    todayBtn.textContent = 'Today';
+
+    todayBtn.addEventListener('click', () => {
+        currentView = 'today';
+        renderTodayView(currentProjects);
+    });
+
+    sidebar.appendChild(todayBtn);
+
+    const overdueBtn = document.createElement('button');
+    overdueBtn.textContent = 'Overdue';
+
+    overdueBtn.addEventListener('click', () => {
+        currentView = 'overdue';
+        renderOverdueView(currentProjects);
+    });
+
+    sidebar.appendChild(overdueBtn);
+
     projects.forEach((project) => {
         const projectBtn = document.createElement('button');
 
+        if (currentView === 'today') {
+            todayBtn.classList.add('active');
+        }
+        if (currentView === 'overdue') {
+            overdueBtn.classList.add('active');
+        }
+
+        if (
+            currentView === 'project' &&
+            project === activeProject
+        ) {
+            projectBtn.classList.add('active');
+        }
+
         projectBtn.classList.add('project-btn');
-        projectBtn.textContent = project.name;
+        projectBtn.textContent = `${project.name} (${project.todos.length})`;
 
         projectBtn.addEventListener('click', () => {
+            currentView = 'project';
             renderProjects(projects, project);
         });
 
@@ -304,6 +513,7 @@ function renderProjectList(projects, activeProject) {
             projectForm.reset();
         });
 
-            sidebar.appendChild(addProjectBtn);
+    sidebar.appendChild(addProjectBtn);
     }
+
 export default renderProjects;
